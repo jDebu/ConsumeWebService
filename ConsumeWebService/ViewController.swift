04f72dcbf756e9b2f6c78a74b4982c7b1927468a
jose manuel <jdebu@joses-Mac-Pro.local>
2016-11-29 22:31:38 -0500

    add readme

--- ConsumeWebService/ViewController.swift
+++ ConsumeWebService/ViewController.swift
@@ -10,8 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var TvNaves: UILabel!
+    
+    @IBAction func ConsumirServicio(_ sender: Any) {
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "http://swapi.co/api/people/1/")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                
+                do {
+                    
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                        
+                        //Implement your logic
+                        print(json)
+                        let nombre = json["name"] as! String
+                        print(nombre)
+                        
+                        let naveArray = json["starships"]
+                        if let jsonArray = naveArray as? NSArray{
+                            jsonArray.enumerateObjects({ object, index, stop in
+                                if let model = jsonArray[index] as? String{
+                                    self.TvNaves.text =  self.TvNaves.text! + model
+                                    
+                                }
+                                
+                            })
+                        }
+                        print(self.TvNaves.text)
+                    }
+                    
+                } catch {
+                    
+                    print("error in JSONSerialization")
+                    
+                }
+                
+                
+            }
+            
+        })
+        task.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "sol.jpeg")!)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+Issues
+===
+Errores al conectarse a un webservice
+
+https://www.youtube.com/watch?v=l1_EQ75FuYA
+
+http://stackoverflow.com/questions/39545603/error-protocol-not-available-dumping-backtrace
+
+Original code
+
+http://mrgott.com/swift-programing/30-work-with-rest-api-in-swift-3-and-xcode-8-using-urlsession-and-jsonserialization
